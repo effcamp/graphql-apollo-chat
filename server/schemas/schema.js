594a83b654ebdfd84acbeebc5478a993e0ba310d
effcamp <fcf.camposx@gmail.com>
2018-01-15 21:55:04 -0200

    More playing around with graphQL queries on the server

--- server/schemas/schema.js
+++ server/schemas/schema.js
@@ -8,23 +8,53 @@ type User {
   username: String
   email: String
   address: Address 
+  posts: [Post]
 }
 
 type Address {
   street: String
 }
 
+type Post {
+  userId: User
+  id: String!
+  title: String
+  body: String
+}
+
 type Query {
   users: [User]
+  user(id: String!): User
+  posts: [Post]
+  
 
 }
 `);
 
 const root = {
-  users: (root, args) =>
-    axios
+  users: async (root, args) => {
+    const users = await axios
       .get(`https://jsonplaceholder.typicode.com/users`)
-      .then((user) => user.data)
+      .then((user) => user.data);
+    const posts = await axios
+      .get(`https://jsonplaceholder.typicode.com/posts`)
+      .then((post) => post.data);
+
+    users.map((user) => {
+      user.posts = posts.filter((post) => post.userId === user.id);
+      return user;
+    });
+
+    return users;
+  },
+  user: (root, args) =>
+    axios
+      .get(`https://jsonplaceholder.typicode.com/users/${root.id}`)
+      .then((user) => user.data),
+  posts: () =>
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts`)
+      .then((post) => post.data)
 };
 
 module.exports = {
